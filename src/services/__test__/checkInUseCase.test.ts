d7eea0dddfc7575de5efa8e8f1b04482b57a7762
eddyzera <esilva644@gmail.com>
2023-12-16 12:48:30 -0300

    test: set date fake

--- src/services/__test__/checkInUseCase.test.ts
+++ src/services/__test__/checkInUseCase.test.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { CheckInUseCase } from '../checkInUseCase'
 import { InMemoryCheckInRepository } from '@/test/inMemoryDataBase/inMemoryCheckInRepository'
 
@@ -9,12 +9,20 @@ describe('User Register Use Case', () => {
   beforeEach(() => {
     checkInRepository = new InMemoryCheckInRepository()
     sut = new CheckInUseCase(checkInRepository)
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
   it('should be able to check in', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
     })
+
+    console.log(`check in => ,`, checkIn.created_at)
 
     expect(checkIn.id).toEqual(expect.any(String))
   })

a492dec64529f314ae1616e300dc5c371489eb33
eddyzera <esilva644@gmail.com>
2023-12-16 12:13:06 -0300

    test: should be able to check in

---
+++ src/services/__test__/checkInUseCase.test.ts
@@ -0,0 +1,21 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { CheckInUseCase } from '../checkinUseCase'
+import { InMemoryCheckInRepository } from '@/test/inMemoryDataBase/inMemoryCheckInRepository'
+
+let checkInRepository: InMemoryCheckInRepository
+let sut: CheckInUseCase
+
+describe('User Register Use Case', () => {
+  beforeEach(() => {
+    checkInRepository = new InMemoryCheckInRepository()
+    sut = new CheckInUseCase(checkInRepository)
+  })
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})

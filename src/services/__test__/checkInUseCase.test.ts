ba3f20098e230312a5ea43f56c8bab27a9d17fc8
eddyzera <esilva644@gmail.com>
2023-12-16 12:40:14 -0300

    test: should not be able to check in in twice on the same day

--- src/services/__test__/checkInUseCase.test.ts
+++ src/services/__test__/checkInUseCase.test.ts
@@ -18,4 +18,18 @@ describe('User Register Use Case', () => {
 
     expect(checkIn.id).toEqual(expect.any(String))
   })
+
+  it('should not be able to check in in twice on the same day', async () => {
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
 })

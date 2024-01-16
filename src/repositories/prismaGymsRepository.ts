7cb9cf0959a14aa7fb27a3251396f993f3db0912
eddyzera <esilva644@gmail.com>
2024-01-16 12:32:01 -0300

    feat: created gyms repository

---
+++ src/repositories/prismaGymsRepository.ts
@@ -0,0 +1,53 @@
+import { Prisma, Gym } from '@prisma/client'
+import { Decimal } from '@prisma/client/runtime/library'
+import { GymsRepositories } from './types/gymsRepositories'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymsRepositories {
+  async findById(gymId: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id: gymId,
+      },
+    })
+
+    return gym
+  }
+
+  async searchManyByTitle(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async findManyNearBy({
+    latitude,
+    longitude,
+  }: {
+    latitude: number
+    longitude: number
+  }) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+}

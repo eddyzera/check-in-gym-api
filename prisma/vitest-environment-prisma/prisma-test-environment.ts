a7605f2439bab66edccfa557cf880c7374adb515
eddyzera <esilva644@gmail.com>
2024-01-18 11:10:58 -0300

    chore: setup to test end to end

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,15 @@
+import type { Environment } from 'vitest'
+
+export default <Environment>(<unknown>{
+  name: 'prisma',
+  transformMode: 'web',
+  async setup() {
+    console.log('Setup')
+
+    return {
+      async teardown() {
+        console.log('Teardown')
+      },
+    }
+  },
+})

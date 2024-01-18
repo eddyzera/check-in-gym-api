50414b97394897075d6c11c8ce7bc04f7751c365
eddyzera <esilva644@gmail.com>
2024-01-18 12:44:39 -0300

    refactor: each test creating a new scheam to data base

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,10 +1,27 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
 import type { Environment } from 'vitest'
+
+// postgresql://docker:docker@localhost:5432/gym-api?schema=public
+
+function generateDataBaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATA_BASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>(<unknown>{
   name: 'prisma',
   transformMode: 'web',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    console.log(`generateDataBaseURL =>`, generateDataBaseURL(schema))
 
     return {
       async teardown() {

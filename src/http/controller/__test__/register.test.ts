0ec123bc101a084a676470e6ebb63107c03eb29f
eddyzera <esilva644@gmail.com>
2024-01-18 13:12:17 -0300

    test: should be able to register

--- src/http/controller/__test__/register.test.ts
+++ src/http/controller/__test__/register.test.ts
@@ -1,5 +1,21 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { describe, expect, it, beforeAll, afterAll } from 'vitest'
+import { app } from '@/app'
 
-test('ok', () => {
-  console.log(`E2E!`)
+describe('Register Controller (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Jonh Doe',
+      email: 'jonh.doe@gmail.com',
+      password: '12345678',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
